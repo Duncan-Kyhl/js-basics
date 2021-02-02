7fb290cf5349968e0bffdfe5d22b03cbadd098a6
Duncan-Kyhl <65314384+Duncan-Kyhl@users.noreply.github.com>
2021-02-02 16:42:09 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+        // put our javascript at the bottom so our
+        // page loads the content, and then the JS
+        console.log('Hello World');
+        
+        // separation of concerns
+        // reference our js file in the script section
+        // instead of writing JS in our HTML
+
+        //
